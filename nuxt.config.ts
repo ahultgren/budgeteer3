86725ed2153a87b238c7ce2c258684815499d9f1
Andreas Hultgren <me@andreashultgren.se>
2024-10-07 15:25:32 +0200

    Fix meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      title: process.env.npm_package_name || '',
+      title: "Budgeeter",
       meta: [
-        { charset: 'utf-8' },
+        { charset: "utf-8" },
         {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1, maximum-scale=1'
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, maximum-scale=1",
         },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: "apple-mobile-web-app-capable", content: "yes" },
         {
-          hid: 'description',
-          name: 'description',
-          content: process.env.npm_package_description || ''
-        }
+          hid: "description",
+          name: "description",
+          content: process.env.npm_package_description || "",
+        },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
   modules: ["@vite-pwa/nuxt"],
