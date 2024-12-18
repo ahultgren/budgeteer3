c95ffa5780d7cef4ce5651118c238c392b08b978
Andreas Hultgren <me@andreashultgren.se>
2024-12-18 15:43:26 +0100

    Use composition api for components

--- components/Overview.vue
+++ components/Overview.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="overview box">
-    <div v-for="item in currentCategories()" :key="item.name" class="overview-category">
+    <div
+      v-for="item in currentCategories(period)"
+      :key="item.name"
+      class="overview-category"
+    >
       <span class="overview-name">{{ item.name }}</span>
       <span class="overview-amount">{{ Math.round(item.amount) }}</span>
       <span class="overview-divider">/</span>
@@ -12,69 +16,15 @@
       <span class="overview-divider">/</span>
       <span class="overview-budget">{{ totalBudget(period) }}</span>
     </div>
-
-    <div class="chart">
-      <svg viewBox="0 0 640 480" class="chart-canvas">
-        <polyline fill="none" stroke="#90f" stroke-width="3" :points="overViewChart()" />
-      </svg>
-    </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import { currentCategories, totalSpent, totalBudget } from "~/assets/scripts";
 
-export default {
-  props: ["period"],
-  methods: {
-    currentCategories() {
-      return currentCategories(this.period);
-    },
-    totalSpent,
-    totalBudget,
-    overViewChart() {
-      const days = this.period.ledger
-        .split(/\n/g)
-        .slice(1)
-        .reduce((arr, line) => {
-          const last = arr[arr.length - 1];
-          const day = line.match(/^#(.+)/);
-
-          if (day) {
-            arr.push({
-              name: day[1],
-              ledger: "",
-            });
-          } else if (last) {
-            last.ledger += line + "\n";
-          }
-
-          return arr;
-        }, [])
-        .map((day, i) => {
-          return [
-            i,
-            totalSpent({
-              ...this.period,
-              ledger: day.ledger,
-            }),
-          ];
-        });
-
-      const max = Math.max.apply(
-        null,
-        days.map(([x, y]) => y)
-      );
-      const scaled = days.map(([x, y]) => [
-        (x / days.length) * 640,
-        480 - (y / max) * 480,
-      ]);
-
-      console.log(scaled, max);
-      return scaled;
-    },
-  },
-};
+const props = defineProps({
+  period: Object,
+});
 </script>
 
 <style lang="less">
