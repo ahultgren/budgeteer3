fd79e27fa6f4ebb77d0564188aa9fc0256a730fb
Andreas Hultgren <me@andreashultgren.se>
2024-12-18 12:37:47 +0100

    Fix manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,24 +23,89 @@ export default defineNuxtConfig({
   modules: ["@vite-pwa/nuxt"],
   pwa: {
     manifest: {
-      name: 'Budgeteer',
-      short_name: 'Budgeteer',
-      lang: 'en',
+      name: "Budgeteer",
+      short_name: "Budgeteer",
+      lang: "en",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#4DBA87",
+      icons: [
+        {
+          src: "icons/icon-48x48.png",
+          sizes: "48x48",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-256x256.png",
+          sizes: "256x256",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
     },
     workbox: {
-      cachingExtensions: '@/plugins/workbox-sync.js',
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,json,svg,webp,vue}'],
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,json,svg,webp,vue}"],
       runtimeCaching: [
         {
-          urlPattern: '/',
-          handler: 'NetworkFirst',
-        }
-      ]
+          urlPattern: "/",
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "html-cache",
+          },
+        },
+        {
+          urlPattern: /\.(?:js|css|html|json|svg|webp|vue)$/,
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "assets-cache",
+          },
+        },
+      ],
     },
     devOptions: {
       enabled: true,
-      type: "module"
-    }
-  }
+      type: "module",
+    },
+  },
 });
